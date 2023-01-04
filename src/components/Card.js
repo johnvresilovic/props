d281ec5dff41ba02337362d500035471ab8cd2ee
John Vresilovic <johnvresilovic@protonmail.com>
2023-01-04 15:55:07 -0500

    done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import CardImage from "./CardImage";
 import CardBody from "./CardBody";
 
-export default function Card1(props) {
+export default function Card1({img, title, text, url}) {
   return (
     <div className="card" style={{width:'18rem'}}>
-    <CardImage img={props.img} />
-    <CardBody title={props.title} text={props.text} url={props.url} />
+    <CardImage img={img} />
+    <CardBody title={title} text={text} url={url} />
   </div>
   );
 }
